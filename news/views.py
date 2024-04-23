da0bb5094113357e21474ab50814fbbf2b7a593e
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-04-23 15:10:49 -0300

    feat: page news details

--- news/views.py
+++ news/views.py
@@ -6,3 +6,8 @@ from news.models import News
 def home(request):
     news_list = News.objects.all()
     return render(request, "home.html", {"news_list": news_list})
+
+
+def news_details(request, id):
+    news = News.objects.get(id=id)
+    return render(request, "news_details.html", {"news": news})
